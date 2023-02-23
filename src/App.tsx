7f1fb5217ee4ad365db691523080759330e4d22f
Akshay Kamble <akshay.kamble@copods.co>
2023-02-23 14:17:47 +0530

    add landing page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Welcome to SaaS Builder 1</h1>
+      <a href="https://www.google.com/">
+        <button style={{ backgroundColor: 'gray' }}>Sign up now</button>
+      </a>
+    </div>
+  )
+}
+
+export default App
